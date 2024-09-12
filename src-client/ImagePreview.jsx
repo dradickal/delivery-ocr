c0b812a650cf6f71cb4a4e617f481446a56dc682
David Radick <dradick@radickalcreations.com>
2024-09-12 15:39:27 -0500

    add userDefinedTime

--- src-client/ImagePreview.jsx
+++ src-client/ImagePreview.jsx
@@ -1,10 +1,46 @@
 import './imagePreview.css';
+import {useState, useRef} from 'react';
 
-function ImagePreview({file, handleRemove}) {
+function ImagePreview({file, handleRemove, handleAddTime}) {
+    const [time, setTime] = useState();
+    const [showTimeInput, setShowTimeInput] = useState(false);
+    const inputRef = useRef(null);
+
+    function saveTime(event) {
+        const target = event.currentTarget;
+        const filename = target.dataset?.filename;
+        const inputTime = inputRef.current.value;
+
+        setTime(inputTime);
+        setShowTimeInput(false);
+
+        handleAddTime(filename, `${inputTime}:00`);
+    }
+
     return (
-        <span className="imagePreview">
+        <span className={showTimeInput ? 'imagePreview inputTime' : 'imagePreview'}>
             <img src={URL.createObjectURL(file)} title={file.name} alt={file.name} />
-            <button className='remove' data-filename={file.name} onClick={handleRemove}>Remove</button>
+            
+            <div className='previewWrapper fileData'>
+                <p>{file.name}</p>
+                <p>{time}</p>
+            </div>
+            
+            <div className='previewWrapper buttons'>
+                <button className='previewButton addTime' onClick={() => setShowTimeInput(true)}>
+                    {time ? 'Edit Image Time' : 'Add Image Time'}
+                </button>
+                <button className='previewButton remove' data-filename={file.name} onClick={handleRemove}>
+                    Remove
+                </button>
+            </div>
+            
+            <div className='previewWrapper timeBlock'>
+                <input type='time' className='imageTime' ref={inputRef} defaultValue={time}/>
+                <button className='previewButton saveTime' data-filename={file.name} onClick={saveTime}>
+                    Set Image Time
+                </button>
+            </div>
         </span>
     )
 }
