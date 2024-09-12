76fd832bfe7f30f9c26a1a666187872452066987
David Radick <dradick@radickalcreations.com>
2024-09-12 16:34:42 -0500

    separate upload image component

--- src-client/ImageUploadForm.jsx
+++ src-client/ImageUploadForm.jsx
@@ -1,50 +1,15 @@
 import './imageUploadForm.css';
-import ImagePreview from './ImagePreview';
-import {useState} from 'react';
+import ImageUploadInput from './ImageUploadInput';
+import { useState } from 'react';
 
-function getIntersectingFilenames(currentFiles, newFiles) {
-    const currentFilenames = new Set(currentFiles.map((file) => file.name));
-    const newFilenames = new Set(newFiles.map((file) => file.name));
-
-    return currentFilenames.intersection(newFilenames);
-}
-
-// filenames is of type Set
-function removeFilesByName(files, filenames) {
-    return filenames.size 
-        ? files.filter(file => !filenames.has(file.name))
-        : files;
-}
 
 function ImageUploadForm() {
     const [files, setFiles] = useState([]);
-    const [ignoredFiles, setIgnoredFiles] = useState([]);
     const [definedTimes, setDefinedTimes] = useState ({});
-    const emptyUpload = 'No files selected for upload';
-
-
-    function handleFileSelection(filesEvent) {
-        console.log("file change triggered");
-        // convert FileList to Array<File>
-        const newFiles = [...filesEvent.target.files];
-        
-        if (files.length) {
-            const duplicateNames = getIntersectingFilenames(files, newFiles);
-            const uniqueFiles = removeFilesByName(newFiles, duplicateNames);
-            setFiles([...files, ...uniqueFiles]);
-            setIgnoredFiles([...duplicateNames]);
-        } else {
-            setFiles(newFiles);
-        }
-    }
 
     function handleSubmit(event) {
         event.preventDefault()
         const data = new FormData(event.target);
-        console.log("event: ", event);
-        console.log("Initial formData: ", data);
-
-        console.log("files state: ", files);
 
         for (const file of files) {
             data.append('images', file);
@@ -52,7 +17,6 @@ function ImageUploadForm() {
 
         data.append("userDefinedTimes", JSON.stringify(definedTimes));
 
-        console.log("Appended formData: ", data);
         const request = new Request("http://localhost:3001/image/upload", {
             method: "POST",
             body: data,
@@ -67,53 +31,27 @@ function ImageUploadForm() {
         })
     }
 
-    function handleAddTime(filename, time) {
-        const newTimes = Object.assign({}, definedTimes);
-        newTimes[filename] = time;
-
-        setDefinedTimes(newTimes);
-    }
-
-    function handleRemove(event) {
-        const target = event.currentTarget;
-        const filename = target.dataset?.filename;
-        const updatedFiles = removeFilesByName(files, new Set([filename]));
-
-        setFiles(updatedFiles);        
-    }
-
     return (
         <>
             <h1>Upload Delivery Actions</h1>
-            <div className="imageUpload">
-                <label className="button" htmlFor="upload-input">Choose Images to Upload</label>
-                <input type="file" id="upload-input" multiple name="images" accept="image/*" onChange={handleFileSelection}/>
-                <br />
-                    <pre>
-                        {!files.length && emptyUpload}
-                        {!!ignoredFiles.length && `Selected files are already included for upload:\n ~ ${ignoredFiles.join("\n ~ ")}`}
-                    </pre>
-                <div id="preview">
-                    {files.map(file => 
-                        <ImagePreview file={file} handleRemove={handleRemove} handleAddTime={handleAddTime} key={file.name}/>
-                    )}
-                </div> 
-            </div>
+            <ImageUploadInput files={files} definedTimes={definedTimes} setFiles={setFiles} setDefinedTimes={setDefinedTimes} />
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>
-                        <input type='radio' name='serviceId' value="1" required defaultChecked/>
+                        <input type='radio' name='serviceId' value="1" required defaultChecked />
                         Grubhub
                     </label>
                     <label>
-                        <input type='radio' name='serviceId' value="2" disabled/>
+                        <input type='radio' name='serviceId' value="2" disabled />
                         DoorDash
                     </label>
                 </div>
+                <br />
                 <label>
-                    Date of Work
+                    Date of Work <br />
                     <input type='date' name='associatedDate'/>
                 </label>
+                <br />
                 <button type="submit">Upload</button>
             </form>
         </>
---
+++ src-client/ImageUploadInput.jsx
@@ -0,0 +1,70 @@
+import ImagePreview from './ImagePreview';
+import {useState} from 'react';
+
+function getIntersectingFilenames(currentFiles, newFiles) {
+    const currentFilenames = new Set(currentFiles.map((file) => file.name));
+    const newFilenames = new Set(newFiles.map((file) => file.name));
+
+    return currentFilenames.intersection(newFilenames);
+}
+
+// filenames is of type Set
+function removeFilesByName(files, filenames) {
+    return filenames.size 
+        ? files.filter(file => !filenames.has(file.name))
+        : files;
+}
+
+function ImageUploadInput({files, definedTimes, setFiles, setDefinedTimes}) {
+    const [ignoredFiles, setIgnoredFiles] = useState([]);
+    const emptyUpload = 'No files selected for upload';
+
+    function handleFileSelection(filesEvent) {
+        console.log("file change triggered");
+        // convert FileList to Array<File>
+        const newFiles = [...filesEvent.target.files];
+        
+        if (files.length) {
+            const duplicateNames = getIntersectingFilenames(files, newFiles);
+            const uniqueFiles = removeFilesByName(newFiles, duplicateNames);
+            setFiles([...files, ...uniqueFiles]);
+            setIgnoredFiles([...duplicateNames]);
+        } else {
+            setFiles(newFiles);
+        }
+    }
+
+    function handleAddTime(filename, time) {
+        const newTimes = Object.assign({}, definedTimes);
+        newTimes[filename] = time;
+
+        setDefinedTimes(newTimes);
+    }
+
+    function handleRemove(event) {
+        const target = event.currentTarget;
+        const filename = target.dataset?.filename;
+        const updatedFiles = removeFilesByName(files, new Set([filename]));
+
+        setFiles(updatedFiles);        
+    }
+
+    return (
+        <div className="imageUploadInput">
+            <label className="button" htmlFor="upload-input">Select Images</label>
+            <input type="file" id="upload-input" multiple name="images" accept="image/*" onChange={handleFileSelection}/>
+            <br />
+                <pre>
+                    {!files.length && emptyUpload}
+                    {!!ignoredFiles.length && `Selected files are already included for upload:\n ~ ${ignoredFiles.join("\n ~ ")}`}
+                </pre>
+            <div id="preview">
+                {files.map(file => 
+                    <ImagePreview file={file} handleRemove={handleRemove} handleAddTime={handleAddTime} key={file.name}/>
+                )}
+            </div> 
+        </div>
+    )
+}
+
+export default ImageUploadInput;
